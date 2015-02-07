effe10cc4a53bdb61b80a9822a9a325f491e281c
Kristen Thyng <kthyng@gmail.com>
2015-02-07 03:45:36 -0600

    added plot_transport script that uses all of the five sections together

---
+++ plot_transport.py
@@ -0,0 +1,229 @@
+'''
+Plot some drifter tracks and location histograms, as determined by input indices.
+Plotted here is a combination of five subsets.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import netCDF4 as netCDF
+import tracpy
+import tracpy.plotting
+import matplotlib as mpl
+import pdb
+import op
+from matplotlib import ticker, colors, cm
+from matplotlib.mlab import find
+import os
+
+
+mpl.rcParams.update({'font.size': 14})
+mpl.rcParams['font.sans-serif'] = 'Arev Sans, Bitstream Vera Sans, Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif'
+mpl.rcParams['mathtext.fontset'] = 'custom'
+mpl.rcParams['mathtext.cal'] = 'cursive'
+mpl.rcParams['mathtext.rm'] = 'sans'
+mpl.rcParams['mathtext.tt'] = 'monospace'
+mpl.rcParams['mathtext.it'] = 'sans:italic'
+mpl.rcParams['mathtext.bf'] = 'sans:bold'
+mpl.rcParams['mathtext.sf'] = 'sans'
+mpl.rcParams['mathtext.fallback_to_cm'] = 'True'
+
+
+whichtime = 'seasonal' # 'seasonal' or 'interannual'
+whichseason = 'summer' # 'winter' or 'summer' for interannual
+howplot = 'log' # 'log' or 'linear'
+
+# loc = 'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/txla_nesting6.nc'
+# grid = tracpy.inout.readgrid(loc, usebasemap=True)
+grid_filename = '/atch/raid1/zhangxq/Projects/txla_nesting6/txla_grd_v4_new.nc'
+vert_filename='/atch/raid1/zhangxq/Projects/txla_nesting6/ocean_his_0001.nc'
+currents_filename = list(np.sort(glob.glob('/atch/raid1/zhangxq/Projects/txla_nesting6/ocean_his_????.nc')))
+if 'grid' not in locals():
+    grid = tracpy.inout.readgrid(grid_filename, vert_filename=vert_filename, usebasemap=True)
+
+if whichtime == 'seasonal':
+
+    cmap = 'YlGn'
+    fname = 'calcs/' + whichtime + '-S.npz'
+
+    S = np.zeros((2,grid['xpsi'].shape[0],grid['xpsi'].shape[1])) # initialize
+
+    fig, axarr = plt.subplots(1,2)
+    fig.set_size_inches(13.675, 6.6125)
+    fig.subplots_adjust(left=0.04, bottom=0.15, right=1.0, top=0.96, wspace=0.07, hspace=0.04)
+
+    for i, ax in enumerate(axarr):
+        # pdb.set_trace()
+
+        # Titles for subplots
+        if i==0:
+            tracpy.plotting.background(grid=grid, ax=ax, mers=np.arange(-100, -80, 2))
+            ax.set_title('Winter')
+            Files = glob.glob('CH/tracks/20??-0[1,2]-*gc.nc')
+            Files.extend(glob.glob('LA/tracks/20??-0[1,2]-*gc.nc'))
+            Files.extend(glob.glob('MX/tracks/20??-0[1,2]-*gc.nc'))
+            Files.extend(glob.glob('NTX/tracks/20??-0[1,2]-*gc.nc'))
+            Files.extend(glob.glob('STX/tracks/20??-0[1,2]-*gc.nc'))
+        elif i==1:
+            tracpy.plotting.background(grid=grid, ax=ax, parslabels=[0,0,0,0], mers=np.arange(-100, -80, 2))
+            ax.set_title('Summer')
+            Files = glob.glob('CH/tracks/20??-0[7,8]-*gc.nc')
+            Files.extend(glob.glob('LA/tracks/20??-0[7,8]-*gc.nc'))
+            Files.extend(glob.glob('MX/tracks/20??-0[7,8]-*gc.nc'))
+            Files.extend(glob.glob('NTX/tracks/20??-0[7,8]-*gc.nc'))
+            Files.extend(glob.glob('STX/tracks/20??-0[7,8]-*gc.nc'))
+
+        if not os.path.exists(fname):
+            for File in Files:
+                print File
+                d = netCDF.Dataset(File)
+                U = d.variables['U'][:]; V = d.variables['V'][:]
+                d.close()
+                Stemp = np.sqrt(op.resize(U, 1)**2 + op.resize(V, 0)**2)
+                S[i,:,:] = S[i,:,:] + Stemp
+
+            # locator = ticker.MaxNLocator(11)
+            # locator.create_dummy_axis()
+            # locator.set_bounds(0, 1) 
+            # levels = locator()
+            # extend = 'max'
+            # H = H/Hmax
+            Smax = 1.
+
+        else:
+            d = np.load(fname); S = d['S']; d.close()
+            Smax = S.max()
+
+        var = (S[i,:,:]/Smax)
+        if howplot=='log':
+            # lev_exp = np.linspace(np.log10(0.1),np.log10(1.0),100)
+            lev_exp = np.linspace(np.log10(0.01),np.log10(0.1),100)
+            levs = np.power(10, lev_exp)
+            ind = var>0.1
+            var[ind] = 0.1
+            mappable = ax.contourf(grid['xpsi'], grid['ypsi'], var, cmap=cmap, levels=levs, norm=colors.LogNorm())
+        elif howplot=='linear':
+            # levs = np.linspace(0,1,100)
+            levs = np.linspace(0,0.1,100)
+            mappable = ax.contourf(grid['xpsi'], grid['ypsi'], var, cmap=cmap, levels=levs, extend='max')
+
+        # ax.contour(grid['xpsi'], grid['ypsi'], var, [0.9], colors='0.2')
+
+        # Outline area
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        CH = np.load(loc_shelftransport + 'calcs/CHpts.npz')['CH']
+        xp = CH[:,0]; yp = CH[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        LA = np.load(loc_shelftransport + 'calcs/LApts.npz')['LA']
+        xp = LA[:,0]; yp = LA[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        MX = np.load(loc_shelftransport + 'calcs/MXpts.npz')['MX']
+        xp = MX[:,0]; yp = MX[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        NTX = np.load(loc_shelftransport + 'calcs/NTXpts.npz')['NTX']
+        xp = NTX[:,0]; yp = NTX[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        STX = np.load(loc_shelftransport + 'calcs/STXpts.npz')['STX']
+        xp = STX[:,0]; yp = STX[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+
+        # Horizontal colorbar below plot
+        cax = fig.add_axes([0.25, 0.075, 0.5, 0.02]) #colorbar axes
+        cb = plt.colorbar(mappable, cax=cax, orientation='horizontal')
+        cb.set_label('Backward transport')
+        levscb = np.hstack((levs[::20],levs[-1]))
+        cb.set_ticks(levscb)
+        cb.set_ticklabels(["%1.4f" % lev for lev in levscb])
+
+    np.savez(fname, S=S, x=grid['xpsi'], y=grid['ypsi'])
+    fig.savefig('figures/' + whichtime + '-' + howplot + '.png', bbox_inches='tight')
+
+
+elif whichtime == 'interannual':
+
+    cmap = 'YlGn'
+
+    shelf_depth = 100
+
+    fname = 'calcs/' + whichtime + '-' + whichseason + '-S.npz'
+
+    S = np.zeros((7,grid['xpsi'].shape[0],grid['xpsi'].shape[1])) # initialize
+
+    fig, axarr = plt.subplots(2,4)
+    fig.set_size_inches(13.4, 6.6125)
+    fig.subplots_adjust(left=0.03, bottom=0.15, right=1.0, top=0.96, wspace=0.03, hspace=0.11)
+
+    for i, ax in enumerate(axarr.flatten()):
+
+        yr = 2004+i
+        if whichseason == 'winter':
+            Files = glob.glob('tracks/' + str(yr) + '-0[1,2]-*gc.nc')
+        elif whichseason == 'summer':
+            Files = glob.glob('tracks/' + str(yr) + '-0[7,8]-*gc.nc')
+
+        # Titles for subplots
+        if i==4:
+            tracpy.plotting.background(grid=grid, ax=ax, mers=np.arange(-100, -80, 2))
+        elif i==7:
+            ax.set_frame_on(False)
+            ax.set_axis_off()
+            continue
+        else:
+            tracpy.plotting.background(grid=grid, ax=ax, mers=np.arange(-100, -80, 2), 
+                merslabels=[0, 0, 0, 0], parslabels=[0, 0, 0, 0])
+
+        ax.set_title(str(yr))
+
+        if not os.path.exists(fname):
+            for File in Files:
+                # print File
+                d = netCDF.Dataset(File)
+                # pdb.set_trace()
+                U = d.variables['U'][:]; V = d.variables['V'][:]
+                d.close()
+                Stemp = np.sqrt(op.resize(U, 1)**2 + op.resize(V, 0)**2)
+                S[i,:,:] = S[i,:,:] + Stemp
+
+            # locator = ticker.MaxNLocator(11)
+            # locator.create_dummy_axis()
+            # locator.set_bounds(0, 1) 
+            # levels = locator()
+            # extend = 'max'
+            # H = H/Hmax
+            Smax = 1.
+
+        else:
+            d = np.load(fname); S = d['S']; d.close()
+            Smax = S.max()
+
+        if howplot=='log':
+            lev_exp = np.linspace(np.log10(0.00001),np.log10(1.0),100)
+            levs = np.power(10, lev_exp)
+        elif howplot=='linear':
+            levs = np.linspace(0,1,100)
+        mappable = ax.contourf(grid['xpsi'], grid['ypsi'], S[i,:,:]/Smax, cmap=cmap, levels=levs, norm=colors.LogNorm())#, extend=extend)
+
+        # Outline area
+        loc_shelftransport = '/home/kthyng/projects/shelf_transport/'
+        CH = np.load(loc_shelftransport + 'calcs/CHpts.npz')['CH']
+        xp = CH[:,0]; yp = CH[:,1]
+        ax.plot(xp, yp, 'k', lw=2, alpha=0.5)
+
+        # Horizontal colorbar below plot
+        cax = fig.add_axes([0.25, 0.075, 0.5, 0.02]) #colorbar axes
+        cb = plt.colorbar(mappable, cax=cax, orientation='horizontal')
+        cb.set_label('Backward transport')
+        levscb = np.hstack((levs[::20],levs[-1]))
+        cb.set_ticks(levscb)
+        cb.set_ticklabels(["%1.4f" % lev for lev in levscb])
+
+    np.savez(fname, S=S, x=grid['xpsi'], y=grid['ypsi'])
+
+    fig.savefig('figures/' + whichtime + '-' + whichseason + '-' + howplot + '.png', bbox_inches='tight')
+
+
+
